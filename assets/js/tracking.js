a0d89fc30fe53998ea81ad3eeffe9d1a481845cf
L. Daniel Nordstrom <d@mrnordstrom.com>
2014-05-18 00:31:35 +0200

    Adding do not track support for Mixpanel tracking.

--- assets/js/tracking.js
+++ assets/js/tracking.js
@@ -13,19 +13,27 @@
   'use strict'
 
   /**
-   * Tracks a page view, along with article details.
+   * Tracks a page view, along with some details of which page.
    *
    * @method
    */
   function initialize() {
-    var articleTitle = document.querySelector('.article-title').textContent
-    var articlePath = location.pathname
+    if (!mixpanel || navigator.doNotTrack) return
 
-    if (articleTitle && articlePath) {
-      mixpanel.track('Page view', { title: articleTitle, path: articlePath })
-    } else {
-      mixpanel.track('Page view', { title: 'Home page', path: '/' })
-    }
+    var path = location.pathname
+    var titleNode = document.querySelector('.article-title')
+    var title = 'Home Page'
+
+    if (titleNode) title = titleNode.textContent
+
+    mixpanel.track('Page View', { title: title, path: path })
+
+    console.log(
+      'Mr. Nordstrom announcement: ' +
+      'This page view was tracked modestly using Mixpanel ' +
+      '(http://mixpanel.com) for statistical purposes. ' +
+      'You may enable "Do Not Track" in your browser to prevent this.'
+    )
   }
 
   if (document.readyState === 'complete') {
